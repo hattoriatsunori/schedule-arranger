4d31f90ba392d7d790f1eb7d03d7a9882d0a9108
Atsunori Hattori <tihove7k@gmail.com>
2023-03-17 16:08:26 +0900

    候補作成処理とリダイレクト処理の共通化

--- routes/schedules.js
+++ routes/schedules.js
@@ -16,20 +16,14 @@ router.get('/new', authenticationEnsurer, (req, res, next) => {
 router.post('/', authenticationEnsurer, async (req, res, next) => {
     const scheduleId = uuidv4();
     const updatedAt = new Date();
-    const schedule = await Schedule.create({
+    await Schedule.create({
       scheduleId: scheduleId,
       scheduleName: req.body.scheduleName.slice(0, 255) || '（名称未設定）',
       memo: req.body.memo,
       createdBy: req.user.id,
       updatedAt: updatedAt
     });
-    const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
-    const candidates = candidateNames.map((c) => { return {
-      candidateName: c,
-      scheduleId: schedule.scheduleId
-    };});
-    await Candidate.bulkCreate(candidates);
-    res.redirect('/schedules/' + schedule.scheduleId);
+    createCandidatesAndRedirect(parseCandidateNames(req), scheduleId, res);
 });
 
 router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
@@ -142,6 +136,21 @@ router.get('/:scheduleId/edit', authenticationEnsurer, async (req, res, next) =>
 
 function isMine(req, schedule) {
   return schedule && parseInt(schedule.createdBy) === parseInt(req.user.id);
+}
+
+async function createCandidatesAndRedirect(candidateNames, scheduleId, res) {
+  const candidates = candidateNames.map((c) => {
+    return {
+      candidateName: c,
+      scheduleId: scheduleId
+    };
+  });
+  await Candidate.bulkCreate(candidates)
+  res.redirect('/schedules/' + scheduleId);
+}
+
+function parseCandidateNames(req) {
+  return req.body.candidates.trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
 }
 
 module.exports = router;
